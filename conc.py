4bdee917d0ddd862f9a664ba60c72b4fe6ec9585
user <user@localhost.localdomain>
2019-06-19 14:30:18 +0530

    Added option to enable/disable follow redirects

--- conc.py
+++ conc.py
@@ -3,19 +3,24 @@ import sys
 import requests
 import urllib3
 import concurrent.futures
+from urllib.parse import urlparse
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 valid_status_codes = [200,201,202,203,204,205,206,
                       300,301,302,303,304,307,308]
 can_open = []
 cannot_open = []
+followRedirects = False
 
 def open_url(url):
     if url == None or url == "":
         return
+    url = url.lower()
+    parse = urlparse(url, "https")
+    url = parse._replace(scheme="https").geturl()
     try:
-        response = requests.get(url.lower(), headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}, timeout=120, verify=False, allow_redirects=True)
-        if response.status_code in valid_status_codes:
+        response = requests.get(url.lower(), headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}, timeout=120, verify=False, allow_redirects=followRedirects)
+        if (response.text != None and response.text != "") or response.status_code in valid_status_codes:
             print(f"{response.url} is accessible")
             can_open.append(response.url)
         else:
@@ -30,6 +35,8 @@ if __name__ == "__main__":
     canFname = args[1]
     cannotFname = args[2]
     workers = int(args[3])
+    if len(args) == 5:
+        followRedirects = bool(args[4])
     urls = None
     if inputFile == "stdin":
         fname = sys.stdin
